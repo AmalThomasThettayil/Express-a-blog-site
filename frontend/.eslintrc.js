c28099662b486e821aa78ca2fbc794b1f45b3f7e
AmalThomasThettayil <102983661+AmalThomasThettayil@users.noreply.github.com>
2022-08-31 22:56:02 +0530

    eslint prettier airbnb

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+export const env = {
+  browser: true,
+  es2021: true,
+};
+export const extends =[
+  'plugin:react/recommended',
+  'airbnb',
+];
+export const overrides = [];
+export const parserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+};
+export const plugins = [
+  'react',
+];
+export const rules = {
+  'no-use-vars': 'warn',
+  'no-console': 'off',
+  'prettier/ prettier': 'error',
+  'func-name': 'off',
+  'object-shorthand': 'off',
+  'class-methods-use-this': 'off',
+};
