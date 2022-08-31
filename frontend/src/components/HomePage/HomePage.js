e817f51490204eefa1b1d9afc32eee8ba7e3c85f
AmalThomasThettayil <102983661+AmalThomasThettayil@users.noreply.github.com>
2022-08-31 12:55:05 +0530

    public,admin navbar

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,34 +1,32 @@
 import React from 'react'
-import poster from "../../img/poster.png"
-
 
 const homePage = () => {
   return (
     <>
-      <section className="pb-10 bg-gray-800">
-        <div className="relative container px-4   mx-auto">
+      <section className="pb-10 bg-yellow-500">
+        <div className="relative container px-4 mx-auto">
           <div className="flex flex-wrap items-center -mx-4 mb-10 2xl:mb-14">
             <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
-              <span className="text-lg font-bold text-blue-400">
+              {/* <span className="text-lg font-bold text-yellow-400">
                 Create posts to educate
-              </span>
-              <h2 className="max-w-2xl mt-12 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading">
-                Pen down your ideas{" "}
-                <span className="text-yellow-500">By creating a post</span>
+              </span> */}
+              <h2 className="max-w-2xl mt-20 mb-12 text-8xl 2xl:text-8xl text-black font-bold font-heading">
+                Stay Curious.{" "}
+                {/* <span className="text-black">By creating a post</span> */}
               </h2>
-              <p className="mb-12 lg:mb-16 2xl:mb-24 text-xl text-gray-100">
-                Your post must be free from racism and unhealthy words
+              <p className="mb-12 lg:mb-16 2xl:mb-24 text-xl text-black font-bold">
+              Discover stories, thinking, and expertise from writers on any topic.
               </p>
               <a
-                className="inline-block px-12 py-5 text-lg text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200"
+                className="inline-block px-12 py-5 text-lg text-white font-bold bg-black hover:bg-yellow-500 hover:text-black rounded-full transition duration-200"
                 href="/"
               >
-                Buy This Course
+                Start reading
               </a>
             </div>
-            <div className="w-full lg:w-1/2 px-4">
+            {/* <div className="w-full lg:w-1/2 px-4">
               <img className="w-full" src={poster} alt={poster} />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
--- frontend/src/components/Navigation/Public/PublicNavbar.js
+++ frontend/src/components/Navigation/Public/PublicNavbar.js
@@ -4,16 +4,15 @@ import {
   MenuIcon,
   XIcon,
   LoginIcon,
-  BookOpenIcon,
 } from "@heroicons/react/outline";
-import { PlusIcon } from "@heroicons/react/solid";
+// import { PlusIcon } from "@heroicons/react/solid";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "Create", href: "/create-post", current: false },
   { name: "Posts", href: "/posts", current: false },
   { name: "Register", href: "/register", current: false },
-  { name: "Login", href: "/login", current: false },
+  // { name: "Login", href: "/login", current: false },
 ];
 
 function classNames(...classes) {
@@ -22,7 +21,7 @@ function classNames(...classes) {
 
 const PublicNavbar = () => {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-yellow-500">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -30,7 +29,9 @@ const PublicNavbar = () => {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="
+                  inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white 
+                  hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -41,17 +42,35 @@ const PublicNavbar = () => {
                 </div>
                 <div className="flex-shrink-0 flex items-center">
                   {/* Logo */}
-                  <BookOpenIcon className="h-10 w-10 text-yellow-200" />
-                </div>
-                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652
+                     2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0
+                      0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                  </svg>
+                  <Link
+                    to="/"
+                    type="button"
+                    className="relative inline-flex items-center  py-2 border border-transparent shadow-sm text-xl 
+                    font-medium rounded-md text-black bg-yellow-500 focus:outline-none"
+                  >
+                    <span>Express</span>
+                  </Link>
+                </div>                
+
+                
+              </div>
+              <div className="flex items-center">
+              <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
                   {navigation.map(item => (
                     <Link
                       key={item.name}
                       to={item.href}
                       className={classNames(
                         item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          ? "bg-black text-white"
+                          : "text-black hover:bg-black hover:text-white",
                         "px-3 py-2 rounded-md text-sm font-medium"
                       )}
                       aria-current={item.current ? "page" : undefined}
@@ -60,13 +79,12 @@ const PublicNavbar = () => {
                     </Link>
                   ))}
                 </div>
-              </div>
-              <div className="flex items-center">
+
                 <div className="flex-shrink-0">
                   <Link
                     to="/login"
                     type="button"
-                    className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+                    className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-yellow-500 hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
                   >
                     <LoginIcon
                       className="-ml-1 mr-2 h-5 w-5"
@@ -75,7 +93,8 @@ const PublicNavbar = () => {
                     <span>Login</span>
                   </Link>
                 </div>
-                <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
+
+                {/* <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
                   <Link
                     to="/create-post"
                     className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-pink-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
@@ -86,7 +105,7 @@ const PublicNavbar = () => {
                     />
                     <span>New Post</span>
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
